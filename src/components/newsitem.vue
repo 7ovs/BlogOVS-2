a72cfacb3087f1811bfe57c94fb44765cf0d223f
Maxim <yakomak137@gmail.com>
2017-10-09 14:32:02 +0300

    Create newsitem.vue

---
+++ src/components/newsitem.vue
@@ -0,0 +1,27 @@
+<template>
+  <li>
+    <h2> {{item.id}})  {{item.title}}</h2>
+    <p class="il">{{ item.content }}</p>
+    <button class="il" @click="deleteItem">X</button>
+  </li>
+</template>
+<script>
+export default {
+  props: ['item'],
+  methods: {
+    deleteItem () {
+      this.$emit('deleteitem', this.item.id)
+    }
+  }
+}
+</script>
+<style scoped>
+  li {
+    list-style-type: none;
+    text-align: center;
+  }
+
+  .il {
+    display: inline-block;
+  }
+</style>

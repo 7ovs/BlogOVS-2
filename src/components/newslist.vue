25d36e3dc6736d7fba486fd83fb28aaa675a1436
Maxim <yakomak137@gmail.com>
2017-10-09 14:32:58 +0300

    Create newslist.vue

---
+++ src/components/newslist.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul>
+    <newsitem v-for="item in items" v-bind:item="item" v-bind:key="item.id" v-on:deleteitem="deleteItem"></newsitem>
+  </ul>
+</template>
+<script>
+import newsitem from './NewsItem'
+export default {
+  name: 'NewsList',
+  props: ['items'],
+  components: {
+    'newsitem': newsitem
+  },
+  methods: {
+    deleteItem (id) {
+      console.log(`delete ${id}`)
+      this.$emit('deleteitem', id)
+    }
+  }
+}
+</script>
+<style scoped>
+  ul {
+    list-style-type: none;
+  }
+</style>
+
